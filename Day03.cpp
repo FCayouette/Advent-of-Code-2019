b707902322a146e68f13e9e6251c1d7d6cca33ea
Francois Cayouette <45644908+FCayouette@users.noreply.github.com>
2019-12-03 16:25:04 -0500

    Day 3 optimized solution
    Instead of a N*Log(N) where is the wire length, uses a N^2 algorithm where N is the number of wire segments. Runs in less than a millisecond on a i7-5930k @ 3.5GHz

--- Day03.cpp
+++ Day03.cpp
@@ -2,25 +2,68 @@
 #include <fstream>
 #include <string>
 #include <vector>
-#include <set>
-#include <map>
+#include <algorithm>
 
-struct Wire
+struct Segment
 {
-	Wire(const std::string& s)
+	int sX, sY, eX, eY, lengthAtStart;
+	char dir;
+
+	void InitSegment(const std::string& s, int& x, int& y, int& length)
 	{
 		dir = s[0];
-		length = atoi(s.substr(1).c_str());
-	}
-	char dir;
-	int length;
-};
+		int dx = 0, dy = 0;
+		switch (dir)
+		{
+		case 'U': dy = 1; break;
+		case 'L': dx = -1; break;
+		case 'R': dx = 1; break;
+		default: dy = -1; break;
+		}
 
-struct Point
-{
-	int x, y;
-	Point(int X=0, int Y=0) : x(X), y(Y) {}
-	bool operator<(const Point& p) const { return x < p.x || (x == p.x && y < p.y); }
+		x += dx;
+		y += dy;
+		sX = x;
+		sY = y;
+		lengthAtStart = ++length;
+		int wireLength = atoi(s.substr(1).c_str()) - 1; // We already shifted by one
+		x += dx * wireLength;
+		y += dy * wireLength;
+		eX = x;
+		eY = y;
+		length += wireLength;
+	}
+
+	bool Intersect(const Segment& s, int& manhattan, int& wireLengthAtCross)
+	{
+		switch (dir)
+		{
+		case 'U':
+		case 'D':
+			if (s.dir == 'U' || s.dir == 'D' ||
+				std::min(sY, eY) > s.sY || std::max(sY, eY) < s.sY ||
+				std::min(s.sX, s.eX) > sX || std::max(s.sX, s.eX) < sX)
+				return false;
+			{
+				int colX = sX, colY = s.sY;
+				manhattan = std::abs(colX) + std::abs(colY);
+				wireLengthAtCross = lengthAtStart + s.lengthAtStart + std::abs(colX - s.sX) + std::abs(colY - sY);
+				return true;
+			}
+			break;
+		default:
+			if (s.dir == 'L' || s.dir == 'R' ||
+				std::min(sX, eX) > s.sX || std::max(sX, eX) < s.sX ||
+				std::min(s.sY, s.eY) > sY || std::max(s.sY, s.eY) < sY)
+				return false;
+			{
+				int colX = s.sX, colY = sY;
+				manhattan = std::abs(colX) + std::abs(colY);
+				wireLengthAtCross = lengthAtStart + s.lengthAtStart + std::abs(colX - sX) + std::abs(colY - s.sY);
+				return true;
+			}
+		}
+	}
 };
 
 int main(int argc, char* argv[])
@@ -35,94 +78,55 @@ int main(int argc, char* argv[])
 	if (!in)
 		return -1;
 
-	// Read data
-	std::string input;
-	in >> input;
-	std::vector<Wire> wire1, wire2;
+	// Process first wire as a list of segments
+	std::string wireDescription;
+	in >> wireDescription;
+
+	std::vector<Segment> firstWire;
 	size_t offset = 0, result = 0;
-
-	do 
-	{
-		result = input.find_first_of(',', offset);
-		if (result != std::string::npos)
-			wire1.emplace_back(input.substr(offset, result - offset));
-		else
-			wire1.emplace_back(input.substr(offset));
-		offset = result + 1;
-
-	} while (result != std::string::npos);
-
-	in >> input;
-	offset = result = 0;
+	int x = 0, y = 0, length = 0;
 	do
 	{
-		result = input.find_first_of(',', offset);
+		result = wireDescription.find(',', offset);
+		Segment segment;
 		if (result != std::string::npos)
-			wire2.emplace_back(input.substr(offset, result - offset));
+			segment.InitSegment(wireDescription.substr(offset, result - offset), x, y, length);
 		else
-			wire2.emplace_back(input.substr(offset));
+			segment.InitSegment(wireDescription.substr(offset), x, y, length);
+		firstWire.push_back(segment);
 		offset = result + 1;
+	} while (result != std::string::npos);
 
+	// Process second wire and check for collisions
+	in >> wireDescription;
+	
+	int manhattan = length, combinePath = length * 2;
+	offset = result = 0;
+	x = y = length = 0;
+	do
+	{
+		result = wireDescription.find(',', offset);
+		Segment segment;
+		if (result != std::string::npos)
+			segment.InitSegment(wireDescription.substr(offset, result - offset), x, y, length);
+		else
+			segment.InitSegment(wireDescription.substr(offset), x, y, length);
+
+		// Check current segment with all the first wire segments
+		for (const Segment& other : firstWire)
+		{
+			int m, c;
+			if (segment.Intersect(other, m, c))
+			{
+				manhattan = std::min(m, manhattan);
+				combinePath = std::min(c, combinePath);
+			}
+		}
+		offset = result + 1;
 	} while (result != std::string::npos);
 
 	in.close();
 
-	std::set<Point> wirePath;
-	std::map<Point, int> wirePathLength;
-	int x = 0, y = 0, length = 0;
-	for (const auto& w : wire1)
-	{
-		int dx = 0, dy = 0;
-		switch (w.dir)
-		{
-		case 'R': dx = 1; break;
-		case 'D': dy = 1; break;
-		case 'L': dx = -1; break;
-		case 'U': dy = -1; break;
-		}
-		for (int i = 0; i < w.length; ++i)
-		{
-			x += dx;
-			y += dy;
-			++length;
-			
-			wirePath.emplace(x, y);
-			Point p(x, y);
-			if (wirePathLength.find(p) == wirePathLength.cend())
-				wirePathLength[p] = length;
-		}
-	}
-
-	x = y = length = 0;
-	int lowest = wirePath.size(), lowestCombine = wirePath.size() * 2;
-	for (const Wire& w : wire2)
-	{
-		int dx = 0, dy = 0;
-		switch (w.dir)
-		{
-		case 'R': dx = 1; break;
-		case 'D': dy = 1; break;
-		case 'L': dx = -1; break;
-		case 'U': dy = -1; break;
-		}
-		for (int i = 0; i < w.length; ++i)
-		{
-			x += dx;
-			y += dy;
-			++length;
-			Point p(x, y);
-			if (wirePath.find(p) != wirePath.cend())
-			{
-				int result = std::abs(x) + std::abs(y);
-				if (result < lowest)
-					lowest = result;
-				int combine = length + wirePathLength[p];
-				if (combine < lowestCombine)
-					lowestCombine = combine;
-			}
-		}
-	}
-
-	std::cout << "Part 1: " << lowest << std::endl << "Part 2: " << lowestCombine << std::endl;
+	std::cout << "Part 1: " << manhattan << std::endl << "Part 2: " << combinePath << std::endl;
 	return 0;
 }
