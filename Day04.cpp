b12659adebbd55f7edcf1d342d2b324a3d7d6e1b
Francois Cayouette <45644908+FCayouette@users.noreply.github.com>
2019-12-04 10:57:56 -0500

    Optimized Day04 solution
    Remove a lot of useless checks by jumping to the next monotonic number in the evaluation sequence

--- Day04.cpp
+++ Day04.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <algorithm>
 
 int main(int argc, char* argv[])
 {
@@ -9,7 +10,7 @@ int main(int argc, char* argv[])
 		std::cout << "Usage: Day04.exe range" << std::endl;
 		return -1;
 	}
-	
+
 	// Range given as input was 235741-706948
 
 	// Process range string
@@ -21,32 +22,33 @@ int main(int argc, char* argv[])
 	int part1 = 0, part2 = 0;
 	for (int i = minRange; i <= maxRange; ++i)
 	{
-		int value[6] = {i/100000, (i%100000)/10000, (i%10000)/1000, (i%1000)/100, (i%100)/10, i%10};
-		bool adjacent = false, monotonic = true;
+		int value[6] = { i / 100000, (i % 100000) / 10000, (i % 10000) / 1000, (i % 1000) / 100, (i % 100) / 10, i % 10 };
+		bool monotonic = true;
 		int histogram[10] = { 0,0,0,0,0,0,0,0,0,0 };
 		for (int j = 0; j < 6; ++j)
 		{
 			++histogram[value[j]];
-			if (j < 5)
+			if (j < 5 && value[j] > value[j + 1])
 			{
-				if (value[j] == value[j + 1])
-					adjacent = true;
-				else if (value[j] > value[j + 1])
-				{
-					monotonic = false;
-					break;
-				}
+				monotonic = false;
+				// Optim: Jump to the next monotonic value
+				for (int k = j + 1; k < 6; ++k)
+					value[k] = value[j];
+				i = 0;
+				for (int k = 0; k < 6; ++k)
+					i = i * 10 + value[k];
+				--i; // Prepare for next interation
+				break;
 			}
 		}
-		
-		if (adjacent && monotonic)
+
+		if (monotonic && std::find_if(histogram, histogram + 10, [](int val) { return val >= 2; }) != histogram + 10 )
 		{
 			++part1;
 			if (std::find(histogram, histogram + 10, 2) != histogram + 10)
 				++part2;
 		}
 	}
-
 	std::cout << "Part 1: " << part1 << std::endl << "Part 2: " << part2 << std::endl;
 	return 0;
 }
