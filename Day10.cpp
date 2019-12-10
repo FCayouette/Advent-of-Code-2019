6d5c65a8f58bb6c1ad8fa45bcba766da936ebd2f
Francois Cayouette <45644908+FCayouette@users.noreply.github.com>
2019-12-10 04:59:27 -0500

    Generic Day10 solution (C++17)
    Removed some inefficiencies, using a vector instead of a set to store visible asteroids. Able to compute part 2 if part 1 answer is less than 200

--- Day10.cpp
+++ Day10.cpp
@@ -3,20 +3,28 @@
 #include <string>
 #include <vector>
 #include <numeric>
-#include <set>
+#include <algorithm>
+
+namespace
+{
+    static int X;
+    static int Y;
+}
+
+using Field = std::vector<std::string>;
 
 struct Asteroid
 {
     int x, y;
     float angle;
-    constexpr Asteroid(int X = 0, int Y = 0, float a = 0.f) : x(X), y(Y), angle(a) {}
+    constexpr Asteroid(int X = 0, int Y = 0, float a = 10.f) : x(X), y(Y), angle(a) {}
     constexpr bool operator <(const Asteroid& p) const { return angle < p.angle; }
 };
 
-float computeAngle(int dY, int dX)
+float computeAngle(int dX, int dY)
 {
     float result;
-    if (dY < 0 && dX == 0) // Would normally return +PI, but we need it to be the small instead
+    if (dY < 0 && dX == 0) // Would normally return +PI, but we need it to be negative PI or less instead
         result = -4.f; 
     else 
         result = std::atan2f((float)-dX, (float)dY);
@@ -24,12 +32,53 @@ float computeAngle(int dY, int dX)
     return result;
 }
 
+int count(const Field& field, int x, int y, std::vector<Asteroid>& asteroids)
+{
+    int seeCount = 0;
+    for (int i = 0; i < Y; ++i)
+        for (int j = 0; j < X; ++j)
+            if (field[i][j] == '#' && !(j == x && i == y))
+            {
+                int dX = j - x;
+                int dY = i - y;
+                bool check = true, insert = false;
+                if (dX == 0)
+                    dY = (dY < 0 ? -1 : 1);
+                else if (dY == 0)
+                    dX = (dX < 0 ? -1 : 1);
+                else
+                {
+                    insert = true;
+                    if (int common = std::gcd(dX, dY); //C++17
+                        common > 1)
+                    {
+                        dX /= common;
+                        dY /= common;
+                        check = true;
+                    }
+                    else check = false;
+                }
+                if (check)
+                {
+                    int mul = 1;
+                    while (field[y + mul * dY][x + mul * dX] != '#')
+                        ++mul;
+                    insert = (y + mul * dY == i && x + mul * dX == j);
+                }
+                if (insert)
+                {
+                    ++seeCount;
+                    asteroids.emplace_back(j, i, computeAngle(dX, dY));
+                }
+            }
+    return seeCount;
+}
 
 int main(int argc, char* argv[])
 {
     if (argc < 2)
     {
-        std::cout << "Usage: Day10.exe []" << std::endl;
+        std::cout << "Usage: Day10.exe [filename]" << std::endl;
         return -1;
     }
     std::ifstream in(argv[1], std::ios::in);
@@ -37,65 +86,25 @@ int main(int argc, char* argv[])
         return -1;
 
     std::string input;
-    std::vector<std::string> field;
+    Field field;
     while (in >> input)
         field.emplace_back(std::move(input));
 
     in.close();
 
-    const int X = field.front().size();
-    const int Y = field.size();
+    X = field.front().size();
+    Y = field.size();
 
     int maxCount = 0, bestX = -1, bestY = -1;
-    std::set<Asteroid> bestAsteroids;
+    std::vector<Asteroid> bestAsteroids;
     for (int y = 0; y < Y; ++y)
         for (int x = 0; x < X; ++x)
             if (field[y][x] == '#')
             {
-                int seeCount = 0;
-                std::set<Asteroid> asteroids;
-                
-                for (int i = 0; i < Y; ++i)
-                    for (int j = 0; j < X; ++j)
-                    {
-                        if (j == x && i == y)
-                            continue;
-                        if (field[i][j] == '#')
-                        {
-                            int dX = j - x;
-                            int dY = i - y;
-                            bool check = true, insert = false;
-                            if (dX == 0)
-                                dY = (dY < 0 ? -1 : 1);
-                            else if (dY == 0)
-                                dX = (dX < 0 ? -1 : 1);
-                            else
-                            {
-                                check = false;
-                                insert = true;
-                                int common = std::gcd(dX, dY); //C++17
-                                while (common > 1)
-                                {
-                                    dX /= common;
-                                    dY /= common;
-                                    common = std::gcd(dX, dY); //C++17
-                                    check = true;
-                                }
-                            }
-                            if (check)
-                            {
-                                int mul = 1;
-                                while (field[y + mul * dY][x + mul * dX] != '#')
-                                    ++mul;
-                                insert = (y + mul * dY == i && x + mul * dX == j);
-                            }
-                            if (insert)
-                            {
-                                ++seeCount;
-                                asteroids.emplace(j, i, computeAngle(dY, dX));
-                            }
-                        }
-                    }
+                std::vector<Asteroid> asteroids;
+                asteroids.reserve(X * Y);
+
+                int seeCount = count(field, x, y, asteroids);
 
                 if (seeCount > maxCount)
                 {
@@ -105,18 +114,21 @@ int main(int argc, char* argv[])
                     std::swap(bestAsteroids, asteroids);
                 }
             }
-    
+
     std::cout << "Best count " << maxCount << " @ " << bestX << ',' << bestY << std::endl;
 
-    if (bestAsteroids.size() >= 200)
+    size_t target = 200;
+    while (bestAsteroids.size() < target)
     {
-        auto iter = bestAsteroids.cbegin();
-        for (int i = 0; i < 199; ++i)
-            ++iter;
-        std::cout << "Part 2: " << iter->x * 100 + iter->y << std::endl;
+        for (const Asteroid& a : bestAsteroids)
+            field[a.y][a.x] = '~';
+        target -= bestAsteroids.size();
+        bestAsteroids.clear();
+        count(field, bestX, bestY, bestAsteroids);
     }
-    else
-        std::cout << "Data set more complex that one received, sorry" << std::endl;
-    
+    std::sort(bestAsteroids.begin(), bestAsteroids.end());
+    --target;
+    std::cout << "Part 2: " << bestAsteroids[target].x * 100 + bestAsteroids[target].y << std::endl;
+
     return 0;
 }
